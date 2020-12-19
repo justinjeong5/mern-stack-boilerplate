af16f758d4e8050bd03bae4e25b2d7f1365ba307
justinjeong <justin.jeong5@gmail.com>
2020-12-19 13:40:44 +0900

    add(server): mongodb
    mongoose, config for mongoURI

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const config = require('./config/key')
 const port = 5000;
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => {
+  console.log('successfully connected to database')
+}).catch((error) => {
+  console.error(error)
+})
 
 app.get('/', (req, res) => {
   res.send('successfully connected server-side app')
