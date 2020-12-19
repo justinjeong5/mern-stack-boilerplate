b080f83af464460f23869e8440504c0c1142f795
justinjeong <justin.jeong5@gmail.com>
2020-12-19 18:35:12 +0900

    initial commit for client
    using npx create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', margin: '3rem auto' }}>
+      <h2>
+        MERN-Stack-Boilerplate made by JustinJeong
+      </h2>
+    </div>
+  );
+}
+
+export default App;
